c63b1c249b7e96a265e8482733c8c99a2476f2b4
KaiserKatze <donizyo@gmail.com>
2018-11-21 21:16:07 +0800

    Update bash script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-DEPLOY_BRANCH_NAME=deploy # CONSTANT VALUE !!!
+# CHANGE THE FOLLOWING VARIABLES
+GITHUB_REPO_URL="https://github.com/donizyo/LaTeX-Travis/"
 
+# !!! DO NOT CHANGE THE FOLLOWING VARIABLES !!!
+GITHUB_FULL_URL="https://donizyo:$GITHUB_TOKEN@${GITHUB_REPO_URL:8}"
+DEPLOY_BRANCH_NAME=deploy
+
+# Routines
 git checkout --orphan "$DEPLOY_BRANCH_NAME"
 git rm --cached --ignore-unmatch -rf -- .
 git add *.pdf
 git add *.log
 git commit --author="traviscibot <>" -m "Publish"
-git push "https://donizyo:$GITHUB_TOKEN@github.com/donizyo/LaTeX-Travis.git" +"$DEPLOY_BRANCH_NAME" # Force push onto deploy branch
+git push "$GITHUB_FULL_URL" +"$DEPLOY_BRANCH_NAME" # Force push onto deploy branch
