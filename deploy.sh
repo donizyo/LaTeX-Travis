fa90806b74d466fa8c174c0b3607ef3798e51c1c
KaiserKatze <donizyo@gmail.com>
2018-11-21 15:58:17 +0800

    Update bash script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git checkout --orphan "$DEPLOY_BRANCH_NAME"
 git add *.pdf
 git add *.log
 git commit --author="traviscibot <>" -m "Publish"
-git push -u origin +"$DEPLOY_BRANCH_NAME" # Force push onto deploy branch
+git push "https://donizyo:$GITHUB_TOKEN@github.com/donizyo/LaTeX-Travis.git" +"$DEPLOY_BRANCH_NAME" # Force push onto deploy branch
