7abff3d1827d960451836db016e7a0058862a850
KaiserKatze <donizyo@gmail.com>
2018-11-22 22:03:22 +0800

    Update bash script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,9 @@ GITHUB_REPO_URL="https://github.com/donizyo/LaTeX-Travis/"
 # !!! DO NOT CHANGE THE FOLLOWING VARIABLES !!!
 GITHUB_FULL_URL="https://donizyo:$GITHUB_TOKEN@${GITHUB_REPO_URL:8}"
 DEPLOY_BRANCH_NAME=deploy
+DEPLOY_AUTHOR_NAME="deploy-bot"
+DEPLOY_AUTHOR_EMAIL="deploy@travis-ci.org"
+DEPLOY_AUTHOR="$DEPLOY_AUTHOR_NAME <$DEPLOY_AUTHOR_EMAIL>"
 
 if [ -z $1 ]; then
   # argument list is needed by 'git add' in the following `else` block
@@ -17,7 +20,7 @@ else
   git checkout --orphan "$DEPLOY_BRANCH_NAME"
   git rm --cached --ignore-unmatch -rf -- .
   git add -- "$@"
-  git commit --author="traviscibot <>" -m "Publish"
+  git commit --author="$DEPLOY_AUTHOR" -m "Publish"
   # Force push onto deploy branch
-  git push "$GITHUB_FULL_URL" +"$DEPLOY_BRANCH_NAME" 
+  git push "$GITHUB_FULL_URL" +"$DEPLOY_BRANCH_NAME"
 fi
