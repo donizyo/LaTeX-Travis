a65795b783b07029626b40e9d0ffff55a32e43dd
KaiserKatze <donizyo@gmail.com>
2018-11-27 16:45:27 +0800

    Bugfix

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # vim: fileencoding=utf-8
 
+# @see https://linux.die.net/man/1/pdflatex
 # `-interaction=nonstopmode` sets the interaction mode to nonstop,
 #     which makes TeX run without requiring feedback from the user,
 #     through the errors.
 # `-halt-on-error` ensures TeX exits with an error code
 #     when an error is encountered during processing,
 #     thus failing the build in Travis CI.
-alias pdflatex='pdflatex -interaction=nonstopmode -halt-on-error'
-find . -type f -iname 'main.tex' -exec pdflatex {} +
+find . -type f -iname 'main.tex' -print0 -exec pdflatex -file-line-error -interaction=nonstopmode -halt-on-error {} +
